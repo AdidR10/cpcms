2859f43dd3973833eb5d09ffbfeda6aa1dff4eee
Taosif <mdtaosifulislam@gmail.com>
2024-03-17 23:14:01 +0600

    updated contest service

--- Frontend/src/app/services/contests.service.ts
+++ Frontend/src/app/services/contests.service.ts
@@ -1,22 +1,28 @@
-// contests.service.ts
-
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service'; 
 
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class ContestsService {
 
-  constructor(private _http:HttpClient) { }
-  getContestList(): Observable<any>{
-    return this._http.get('http://localhost:3010/api/v1/contests');
-    // return this._http.get('https://codeforces.com/api/contest.list?CF=true');
+  constructor(private _http: HttpClient, private authService: AuthenticationService) { }
+
+  private getHeaders(): HttpHeaders {
+    const token = this.authService.getAuthToken();
+    return new HttpHeaders().set('x-auth-token', token ? token : '');
   }
-  addContest(data: any): Observable<any>{
-    return this._http.post('http://localhost:3010/api/v1/contests',data);
+
+  getContestList(): Observable<any> {
+
+    // Make GET request with headers
+    return this._http.get('http://localhost:3010/api/v1/contests', { headers: this.getHeaders() });
+  }
+
+  addContest(data: any): Observable<any> {
+    return this._http.post('http://localhost:3010/api/v1/contests', data);
   }
 }
